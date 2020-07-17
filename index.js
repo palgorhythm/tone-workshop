1436dad2b4a7e349cbefc268645c07c585b97317
palgorhythm <jacob@mothership.com>
2020-07-16 19:45:12 -0700

    remove parcel stuff, works now

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-import * as Tone from "tone";
-
 var synth = null
 
 var scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 for (var i  = 0; i < scale.length; i ++){
+  // get the current note of the scale
   var currentNote = scale[i]
+  
+  // create a button element
   var button = document.createElement('button')
-  createButtonHandler(button, currentNote)
+
+  // set the text of the button
+  button.innerHTML = currentNote
+
+  // create a handler for button presses
+  var buttonHandler = createButtonHandler(currentNote)
+  // make the handler run when you click the button
+  button.addEventListener('click', buttonHandler)
+
+  // actually draw the button onto the page
+  document.body.appendChild(button);
 }
 
 
-function createButtonHandler(button, note, octave = 4)  {
-  function handler() {
+function createButtonHandler(note, octave = 4)  {
+  function handler () {
     console.log(`clicked the ${note} button!`)
     if(!synth){
       synth = new Tone.Synth().toMaster(); 
     }
     synth.triggerAttackRelease(`${note}${octave}`, "8n");
   }
-  button.addEventListener('click', handler)
-  console.log(button)
+  return handler
 }
