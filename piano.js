6d1a024374d00b31c740b62cc4f5a494f02117ce
palgorhythm <jacob@mothership.com>
2020-08-08 16:56:16 -0700

    finished workshop code, time to clean up

---
+++ piano.js
@@ -0,0 +1,35 @@
+var synth = null
+
+var scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+for (var i  = 0; i < scale.length; i ++){
+  // get the current note of the scale
+  var currentNote = scale[i]
+  
+  // create a button element
+  var button = document.createElement('button')
+
+  // set the text of the button
+  button.innerHTML = currentNote
+
+  // create a handler for button presses
+  var buttonHandler = createButtonHandler(currentNote)
+  // make the handler run when you click the button
+  button.addEventListener('click', buttonHandler)
+
+  // actually draw the button onto the page
+  document.body.appendChild(button);
+}
+
+
+// output a function that we call every time a button is clicked
+function createButtonHandler(note, octave = 4)  {
+  function handler () {
+    console.log(`clicked the ${note} button!`)
+    if(!synth){
+      synth = new Tone.Synth().toDestination(); 
+    }
+    synth.triggerAttackRelease(`${note}${octave}`, "8n");
+  }
+  return handler
+}
