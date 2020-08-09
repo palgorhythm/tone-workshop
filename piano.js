a665279fcd5c98587d0e280ba676a2e06da9e4df
palgorhythm <jacob@mothership.com>
2020-08-08 18:02:46 -0700

    done?

--- piano.js
+++ piano.js
@@ -1,19 +1,19 @@
-var synth = null
+let synth = null
 
-var scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+let scale = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
-for (var i  = 0; i < scale.length; i ++){
+for (let i  = 0; i < scale.length; i ++){
   // get the current note of the scale
-  var currentNote = scale[i]
+  let currentNote = scale[i]
   
   // create a button element
-  var button = document.createElement('button')
+  let button = document.createElement('button')
 
   // set the text of the button
   button.innerHTML = currentNote
 
   // create a handler for button presses
-  var buttonHandler = createButtonHandler(currentNote)
+  let buttonHandler = createButtonHandler(currentNote)
   // make the handler run when you click the button
   button.addEventListener('click', buttonHandler)
 
